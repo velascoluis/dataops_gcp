73ce3ac6d564ad94ad0b436679ab6c28bfe69782
Luis Velasco <velascoluis@google.com>
2024-10-31 12:38:58 +0100

    dummy

--- etl/dags/airline_dag_sql.py
+++ etl/dags/airline_dag_sql.py
@@ -54,6 +54,7 @@ with models.DAG(
     `{project_id}.{dataset_id}.{dim_airport_table}` AS a ON df.departure_airport_key = a.airport_key;"""
 
 
+
     QUERY_STEP_3 = """
     SELECT
         a.airport_name,
